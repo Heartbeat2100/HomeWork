f0392a86f7107c607e7dff3b6b607eb8038ece8c
Heartbeat2100 <beat210760@gmail.com>
2022-09-13 13:56:17 +1000

    Добавлена вторая домашняя работа

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ let user ='user name';
 console.log(user);
 user= 5;
 console.log(user);
+
+// 2 Домашка
+let array = [1,2,3,4,5];
+console.log(array[2]);
+
+const phone = {
+    color: '#fff',
+    model: 'iphone', 
+    price: 50000 
+};
+for(let key in phone){
+    console.log(phone[key]);
+}
